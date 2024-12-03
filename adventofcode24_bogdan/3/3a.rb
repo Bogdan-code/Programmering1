31aa593dd196a90a480e2535c046faa5ba3e743a
Bogdan-code <gaminghavet@hotmail.com>
2024-12-03 15:37:44 +0100

    commit

--- adventofcode24_bogdan/3/3a.rb
+++ adventofcode24_bogdan/3/3a.rb
@@ -2,7 +2,7 @@ input = File.read("input.txt")
 
 i = 0
 str = ""
-
+doing = true
 sum = 0
 
 class String
@@ -15,10 +15,9 @@ while i != input.length
   arr = []
   str2 = ""
   str += input[i]
-  if str == "mul("
+  if str == "mul(" && doing == true
     i += 1
     while input[i] != ")" 
-      p str
       if input[i].numeric? == true || input[i] == ","   
         str2 += input[i]
       else 
@@ -27,14 +26,19 @@ while i != input.length
       i+=1
       if input[i] == ")"
         arr = str2.split(",")
+        p arr
         sum += arr[0].to_i * arr[1].to_i
       else 
         next
       end
     end
-    
-
-  elsif str.length > 3 
+  elsif str.length > 3  
+    if str == "do()"
+      doing = true
+    elsif str + input[i+1]+input[i+2]+input[i+3] == "don't()"
+      doing = false
+    end
+    i -= 3
     str = ""
   end
   i += 1
